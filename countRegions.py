9b9026c40a980139bafcde05185238e3c48dfc82
Steffen <steffen.albrecht@desy.de>
2018-06-16 15:23:49 +0200

    added Color to Output and changed Output itself, so it is more meaningful

--- countRegions.py
+++ countRegions.py
@@ -4,6 +4,20 @@ import os, sys, csv, collections, numpy, math
 from ROOT import gROOT, gSystem,TF1, TFile
 import ROOT as rt
 
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+referenceHistPath = 'tau21sel/M_jj_AK8'
+# referenceHistPath = 'invMAk4sel_1p0/M_jj_AK8'
+   
 def magnitude(x):
     return int(math.floor(math.log10(x)))
 
@@ -19,51 +33,40 @@ def CountRegion(filename):
     # DataFile = TFile(path+"/uhh2.AnalysisModuleRunner.Data.DATA.root")
     DataFile = TFile(filename)
     gROOT.ProcessLine( "gErrorIgnoreLevel = 0;")
-    referenceHistPath = 'tau21sel/M_jj_AK8'
-    # referenceHistPath = 'invMAk4sel_1p0/M_jj_AK8'
     IntegralMjj=DataFile.Get(referenceHistPath).Integral()
     print 'Integral_MjjAK8=',IntegralMjj
 
-    return IntegralMjj
+    return float(int(IntegralMjj))
     
 
 if(__name__=='__main__'):
     channels=['WPWP','WPWM','WMWM','WPZ','WMZ','ZZ']
-    # tau21selections=['tau21_35']
-    tau21selections=['tau21sel_45','tau21sel_35']
-    # tau21selections=['tau21sel_45']
     RegionPaths=[   '/nfs/dust/cms/user/albrechs/UHH2_Output/SignalRegion',
-                    # '/nfs/dust/cms/user/albrechs/UHH2_Output/LOWSidebandRegion',
-                    '/nfs/dust/cms/user/albrechs/UHH2_Output/HIGHSidebandRegion'
+                    '/nfs/dust/cms/user/albrechs/UHH2_Output/SidebandRegion'
     ]
-    for tau21sel in tau21selections:
-        csv_out=open('%s_RegionComparison.csv'%tau21sel,'wt')
-        csvwriter=csv.DictWriter(csv_out,fieldnames=['channel','NSignal','NSideband','N','NSideband/NSignal','NSideband/N'])
-        csvwriter.writeheader()
-
-        for channel in channels:
-            filename='uhh2.AnalysisModuleRunner.MC.MC_aQGC_%sjj_hadronic.root'%channel
-
-
-            N_Events=[]
-            for path in RegionPaths:
-                N_Events.append(CountRegion(path+'/'+tau21sel+'/'+filename))
-            
-                # print 'Signal/LowSideband:',N_Events[0]/N_Events[1]
-            N_Signal=N_Events[0]+N_Events[1]    
-            csvwriter.writerow({'channel':channel,
-                                'NSignal':N_Events[0],
-                                'NSideband':N_Events[1],
-                                'N':N_Signal,
-                                'NSideband/NSignal':N_Events[1]/N_Events[0],
-                                'NSideband/N':N_Events[1]/N_Signal})
-
-            print '##################################################################'
-            print 'Channel:',channel
-            print 'For N-Subjettiness-Selection (0<tau21<0.%s)'%tau21sel.split('_')[-1]
-            print 'N_Signal in SignalRegion:',N_Events[0],'and in SidebandRegion:',N_Events[1],' (both N_Signal:',N_Signal,')'
-            print 'N_Signal_Sideband/N_Signal:',N_Events[1]/N_Signal
-            print 'N_Signal_Signal/N_Signal:',N_Events[0]/N_Signal
-            print 'N_Signal_Signal/N_Signal_Sideband:',N_Events[0]/N_Events[1]
-            print '##################################################################'
-            print ''
+    csv_out=open('RegionComparison_%s.csv'%referenceHistPath.split('/')[0],'wt')
+    csvwriter=csv.DictWriter(csv_out,fieldnames=['channel','NSignal','NSideband','N','NSideband/NSignal','NSideband/N'])
+    csvwriter.writeheader()
+    
+    for channel in channels:
+        print '##################################################################'
+        print 'Channel:',channel
+        
+        filename='uhh2.AnalysisModuleRunner.MC.MC_aQGC_%sjj_hadronic.root'%channel
+        N_Events=[]
+        for path in RegionPaths:
+            print path
+            N_Events.append(CountRegion(path+'/'+filename))
+        N=N_Events[0]+N_Events[1]    
+        csvwriter.writerow({'channel':channel,
+                            'NSignal':N_Events[0],
+                            'NSideband':N_Events[1],
+                            'N':N,
+                            'NSideband/NSignal':N_Events[1]/N_Events[0],
+                            'NSideband/N':N_Events[1]/N})
+        
+        print 'N_Signal in SignalRegion:',N_Events[0],'and in SidebandRegion:',N_Events[1],' (both N_Signal:',N,')'
+        print 'N_Signal_Signal/N_Signal:',str(N_Events[0]/N)
+        print color.BOLD+color.YELLOW+'N_Signal_Sideband/N_Signal_Signal:',str(N_Events[1]/N_Events[0])+color.END
+        print '##################################################################'
+        print ''
