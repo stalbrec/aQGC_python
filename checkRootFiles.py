553106c9a71ddaae3e13426a7e0204f8dbd41ee9
Steffen <steffen.albrecht@desy.de>
2018-07-24 13:46:27 +0200

    can now check all Steps in production cycle

--- checkRootFiles.py
+++ checkRootFiles.py
@@ -25,7 +25,21 @@ def checkChannelFiles(channel):
     gROOT.SetBatch(True)
     gROOT.ProcessLine( "gErrorIgnoreLevel = 2001;")
     scriptdir=os.getcwd()
-    path="/nfs/dust/cms/user/albrechs/production/ntuples/split_LHE/"
+    # path="/nfs/dust/cms/user/albrechs/production/ntuples/split_LHE/"
+    # path="/nfs/dust/cms/user/albrechs/production/ntuples/GENSIM/"
+    path="/nfs/dust/cms/user/albrechs/production/ntuples/NT_"
+
+    # Events=500
+    Events=625
+    doubtful_size=47000
+    # doubtful_size=500000000
+    file_prefix='Ntuple_'+channel+'_'
+    # file_prefix='GEN_SIM_'+channel+'_'
+    # file_prefix=channel+'_LHE_'
+    file_suffix='.root'
+
+    print('path: '+path)
+    print('Events: %s, Prefix: %s, Suffix: %s'%(Events,file_prefix,file_suffix))
 
 
     #extracting expected Number of Reweighting Points
@@ -37,7 +51,10 @@ def checkChannelFiles(channel):
             N_Reweight_expected+=int(row['Npoints'])
     os.chdir(path+channel)
     files=glob.glob("*.root")
-    failed_jobs=range(0,100)
+    if('Ntuple' in file_prefix):
+        failed_jobs=range(0,800)
+    else:
+        failed_jobs=range(0,len(files))
         
     print('%s (expected #ReweightingPoints %i):'%(channel,N_Reweight_expected))
 
@@ -46,21 +63,28 @@ def checkChannelFiles(channel):
         os.chdir(scriptdir)
         return -1
 
-    print('number of files:',len(files))
+    print('number of files:'+str(len(files)))
 
     for i in range(len(files)):
         size=os.stat(files[i]).st_size
-        file_index=files[i][files[i].find("LHE_")+4:files[i].find(".root")]
+        file_index=files[i][files[i].find(file_prefix)+len(file_prefix):files[i].find(file_suffix)]
         #if file is smaller than 47kB job definitely failed
-        if(size<47000):
+        if(size<doubtful_size):
             # failed_jobs.append(int(file_index))
             update_progress(i+1,len(files))
             continue
-        rootFile=TFile(files[i])
+        if('Ntuple' in file_prefix):
+            failed_jobs.remove(int(file_index))
+            continue
+        rootFile=TFile(files[i])        
         gROOT.ProcessLine( "gErrorIgnoreLevel = 1001;")
         wgt_hist=TH1F('wgt_hist','systweights',1,0,100)
+        if(rootFile.Get('Events')==None):
+            update_progress(i+1,len(files))
+            continue
+            
         rootFile.Get('Events').Draw('wgt>>wgt_hist')
-        N_Reweight_actual=int(wgt_hist.GetEntries())/500
+        N_Reweight_actual=int(wgt_hist.GetEntries())/625
         
         # if('WPWP' in channel):
         #     N_Reweight_actual-=882
@@ -90,7 +114,7 @@ def checkChannelFiles(channel):
 if(__name__=='__main__'):
     print('checking Files of following channels:')
     # channels=["WPWP","WPWM","WMWM","WPZ","WMZ","ZZ"]
-    channels=['WPWP']
+    channels=['ZZ']
     # channels=["WPZ","WMZ","ZZ"]
     # channels=["WPZ","WMZ","ZZ"]
     # channels=['WPZ']
