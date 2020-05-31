8c000def9145c0b6d5b0ba6b6349a87ac6cd7122
Steffen Albrecht <steffen.albrecht@desy.de>
2020-05-31 20:33:09 +0200

    cleaned script to for creation og combine input files

--- produce_combine_input.py
+++ produce_combine_input.py
@@ -1,27 +1,17 @@
-import ParSet,subprocess,os,shutil,csv,glob
+import ParSet
 
 
 if(__name__=="__main__"):
     path='/nfs/dust/cms/user/albrechs/CMSSW_8_1_0/src/DijetCombineLimitCode/input'
     
-    # dim8op=["T0"]
-    # dim8op=["S0","S1","M0","M1","M6","M7","T0","T1","T2"]
-    dim8op=["S0","S1","M0","M1","M2","M3","M4","M5","M6","M7","T0","T1","T2","T5","T6","T7","T8","T9"]
+    dim8op=["S0","S1","S2","M0","M1","M2","M3","M4","M5","M7","T0","T1","T2","T5","T6","T7","T8","T9"]
     
-    # channels=["WPWP","WPWM","WMWM","WPZ","WMZ","ZZ"]
     region='SignalRegion'
-    # channels=['VV']
-    channels=['VV','ssWW','ZZ']
-    # channels=['VV','ssWW','WZ','WPWP','WPWM','WMWM','WPZ','WMZ','ZZ']
-    # cuts=['detaAk4sel','invMAk4sel_1p0','invMAk4sel_1p2','invMAk4sel_1p5_allcuts']
+    channels=['ZZ']
     cuts=['VVRegion','invMAk4sel_1p0']
     for channel in channels:
         for cut in cuts:
-            for op in dim8op:
-                # plot_dir='output/plots/Fits/%s/%s/'%(cut,op)
-                # if not os.path.exists(plot_dir):
-                #     os.makedirs(plot_dir)
-                
+            for op in dim8op:                
                 print('+++++++++++writing RootFiles - %s - %s+++++++++++'%(op,cut))
                 current_Set=ParSet.Set(op,channel,cut,region)
                 current_Set.CombinedRootFiles(path)
